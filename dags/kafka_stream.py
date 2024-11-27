af111acf2dd4635e39d005aa216e0a26379290b7
Robert <robkhz@umich.edu>
2024-11-27 05:29:02 -0500

    Added Kafka data streaming, Airflow DAG, and Docker services

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -37,20 +37,33 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=3))
+    # print(json.dumps(res, indent=3))
+
+    producer = KafkaProducer(bootstrap_servers=["broker:29092"], max_block_ms=5000)
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60:  # 1 Minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+            producer.send("users_created", json.dumps(res).encode("utf-8"))
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            continue
 
 
-# with DAG("user_automation",
-#          default_args=default_args,
-#          schedule_interval="@daily",
-#          catchup=False) as dag:
-
-#     streaming_task = PythonOperator(
-#         task_id="stream_data_from_api",
-#         python_callable=stream_data
-#     )
-
-stream_data()
+with DAG(
+    "user_automation",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    streaming_task = PythonOperator(
+        task_id="stream_data_from_api", python_callable=stream_data
+    )
